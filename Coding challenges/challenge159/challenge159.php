fa67f2f40de794b5e519adb62206bca8cf0af026
Giovanni Colasante <giovanni.colasante@gmail.com>
2015-08-25 23:17:30 +0200

    update

--- Coding challenges/challenge159/challenge159.php
+++ Coding challenges/challenge159/challenge159.php
@@ -1,42 +1,6 @@
 <?php
 include_once 'vendor/simple_html_dom.php';
 
-function sampling($chars, $size, $combinations = array()) {
-
-    # if it's the first iteration, the first set 
-    # of combinations is the same as the set of characters
-    if (empty($combinations)) {
-        $combinations = $chars;
-    }
-
-    # we're done if we're at size 1
-    if ($size == 1) {
-        return $combinations;
-    }
-
-    # initialise array to put new values in
-    $new_combinations = array();
-
-    # loop through existing combinations and character set to create strings
-    foreach ($combinations as $combination) {
-        foreach ($chars as $char) {
-            $new_combinations[] = $combination . $char;
-        }
-    }
-
-    # call same function again for the next iteration
-    return sampling($chars, $size - 1, $new_combinations);
-
-}
-
-// example
-echo date ('s') . PHP_EOL;
-$chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-$output = sampling($chars, 6);
-echo date ('s') . PHP_EOL;
-//var_dump($output);
-
-
 /*
 * paste session id value es: PHPSESSID=t4p4a7jcifmb0qp90k5mrqr534
 * so only : t4p4a7jcifmb0qp90k5mrqr534*
