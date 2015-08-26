c716d26de2e395f84b5b5499651d3fe4f6c17985
root <root@localhost.localdomain>
2015-08-26 10:50:30 +0200

    update

--- Coding challenges/challenge159/challenge159.php
+++ Coding challenges/challenge159/challenge159.php
@@ -1,11 +1,36 @@
 <?php
 include_once 'vendor/simple_html_dom.php';
 
+function checkSha($toHash){
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=hash", $username, $password);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$sql = "SELECT string FROM hash WHERE sha1 = '" . $toHash ."'" ;
+		foreach ($conn->query($sql) as $result)
+		{
+			return $result['string'];
+		}
+	
+	    }
+	catch(PDOException $e)
+	    {
+	    echo $sql . PHP_EOL . $e->getMessage();
+	    }
+
+	$conn = null;
+}
+
 /*
 * paste session id value es: PHPSESSID=t4p4a7jcifmb0qp90k5mrqr534
 * so only : t4p4a7jcifmb0qp90k5mrqr534*
 */
-$sessionId = '06b1sjb1k926poqghrlljtbre5';
+$sessionId = '65b9gha3718oo3lu328o8o0b44';
 $toHash = '';
 
 // create curl resource
@@ -37,10 +62,11 @@ $toHash = preg_replace($patterns, '', $toHash);
 $toHash = trim($toHash); 
 
 // Hash the message
-$tmp = 'd94b40537b362801da53091372967c34ad9d9161';
-echo $toHash; 
+$result = checkSha($toHash);
 
-die();
+if (empty($result)) {
+	die('Non trovata. - '. $toHash .PHP_EOL);
+}
 
 // Send hash
 curl_setopt($ch, CURLOPT_URL, "http://ringzer0team.com/challenges/159/" . $result);
